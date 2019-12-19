3fcc702c8ba4beaf9063f287aeb0c782ce93201d
gaowei <1qaz2wsx>
2019-12-19 21:26:55 +0800

    feign starter

--- distribution-service/src/main/java/com/jq/distribution/Application.java
+++ distribution-service/src/main/java/com/jq/distribution/Application.java
@@ -16,7 +16,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @Slf4j
 @EnableFeignClients(basePackages = {"com.jq"}, defaultConfiguration= FeignConfig.class)
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.jq"})
 public class Application {
 
     public static void main(String[] args) {
