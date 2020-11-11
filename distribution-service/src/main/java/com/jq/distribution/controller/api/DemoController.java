5c7c4da1cdad7dd7a31d2e88dd10b8869cb57a49
gaowei <gaowei@fengjinggroup.com>
2020-11-11 13:39:42 +0800

    debug

--- distribution-service/src/main/java/com/jq/distribution/controller/api/DemoController.java
+++ distribution-service/src/main/java/com/jq/distribution/controller/api/DemoController.java
@@ -3,6 +3,7 @@ package com.jq.distribution.controller.api;
 import com.jq.distribution.service.ExampleInnerService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import javax.annotation.Resource;
 
 @Slf4j
 @RestController
+@RefreshScope
 public class DemoController {
 
     @Value("${timeout:100}")
